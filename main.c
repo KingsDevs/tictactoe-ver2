d3055a2ce7ea997badd593a278451025b349a469
KingsDevs <sunnyckair@gmail.com>
2021-11-28 23:19:34 +0800

    hays nakilmot kog commet daghan na

--- main.c
+++ main.c
@@ -49,23 +49,85 @@ void print_board(char ** board, size_t size)
     print_line(size);
 }
 
-void update_board(char ** board, size_t size, char player)
-{
-    unsigned int row, col;
-    printf("Input <row, column> :\n>> ");
-    scanf("%d%d", &row, &col);
 
-    board[row-1][col-1] = player;
+void update_board(char ** board, size_t size, char player, int * ai_move)
+{      
+    if(ai_move == NULL)
+    {
+        unsigned int row, col;
+        printf("Input <row, column> :\n>> ");
+        scanf("%d%d", &row, &col);
+        board[row-1][col-1] = player;
+    }
+    else
+    {
+       board[ai_move[0]][ai_move[1]] = player;
+    }
     print_board(board, size);
 }
 
+bool check_won(char ** board, char player, size_t size)
+{
+    short int score = 0;
+    //horizontal
+    for (size_t i = 0; i < size; i++)
+    {
+        score = 0;
+        if (board[i][0] == player)
+        {
+            score++;
+            for (size_t j = 1; j < size; j++)
+            {
+                if (board[i][j] == player)
+                    score++;
+            }
+            if (score == size)
+                return true;
+        }
+    }
+
+    //vertical
+    for (size_t i = 0; i < size; i++)
+    {
+        score = 0;
+        for (size_t j = 0; j < size; j++)
+        {
+            if (board[j][i] == player)
+                score++;
+        }
+        if (score == size)
+            return true;
+        
+    }
+    
+    //front slash
+    
+    
+    for (size_t i = 0; i < size; i++)
+    {
+        if (board[i][i] == player)
+            score++;
+    }
+
+    if (score == size)
+        return true;
+
+
+    
+    return false;
+}
 int main()
 {
     size_t size = 3;
     char ** board = create_board(size);
     print_board(board, size);
 
-    update_board(board, size, PLAYER1);
+    update_board(board, size, PLAYER1, NULL);
+    update_board(board, size, PLAYER1, NULL);
+    update_board(board, size, PLAYER1, NULL);
+
+    printf("%d", check_won(board, PLAYER1, size));
+    
 
     return 0;
 }
